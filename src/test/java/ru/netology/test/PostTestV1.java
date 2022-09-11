159bf2ff69de1eea1ee1e75bf3e089c591b36ed4
Ася Тамонова <asyatamoniva@192.168.1.161>
2022-09-11 11:24:36 +0300

    Fail Test

--- src/test/java/ru/netology/test/PostTestV1.java
+++ src/test/java/ru/netology/test/PostTestV1.java
@@ -19,7 +19,7 @@ public class PostTestV1 {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
